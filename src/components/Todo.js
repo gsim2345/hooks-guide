c35040c25b62793c4f29297a551382381d981cc3
gsim2345 <gb@gabriellasimon.me>
2019-05-01 20:37:03 +0200

    Added useEffect to send get request to server

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 
 
@@ -22,6 +23,30 @@ const Todo = props => {
     //const [todoState, setTodoState] = useState({ userInput: '', todoList: []});
     
 
+    // pass a function that needs to be executed
+    // will be executed when this component runs for the first time
+    // if we call a function just out of useEffect that effects state, will be called during the render cycle, and it's bad for performance reasons. 
+    // instead call as a callback function passed to useEffect
+    // it runs after every render cycle, not only once like componentDidMount
+    // after result arrived, and we change state => rerenders again, ==> sends new request ==> infinite loop
+    // can add a second argument to solve this ==> an array of values
+    // we can look at those values, and only if one of the values of the array has changed, should the effect run. 
+    // if you want to run an effect only on mounting, pass an empty array []
+    // passing in empty array ==> like replicating componentDidMount
+    useEffect(() => {
+        axios.get('https://test-hooks-7593f.firebaseio.com/todos.json')
+        .then(result => {
+            console.log(result);
+            const todoData = result.data;
+            const todos = [];
+            for (const key in todoData) {
+                todos.push({id: key, name: todoData[key].name})
+            }
+            console.log(todos);
+            setTodoList(todos);
+        })
+    }, [todoName] );
+
     const inputChangeHandler = (event) => {
         // we execute this function to update the state with passing in the updated state
         setTodoName(event.target.value);
@@ -32,6 +57,13 @@ const Todo = props => {
     const todoAddHandler = () => {
         setTodoList(todoList.concat(todoName));
         //setTodoState({userInput: todoState.userInput, todoList: todoState.todoList.concat(todoState.userInput)});
+        axios.post('https://test-hooks-7593f.firebaseio_.com/todos.jso', {name: todoName})
+        .then(res => {
+            console.log(res);
+        })
+        .catch(err => {
+            console.log(err);
+        });
     }
 
     return <React.Fragment>
@@ -39,7 +71,7 @@ const Todo = props => {
         <button type="button" onClick={todoAddHandler}>Add</button>
         <ul>
             {todoList.map(todo => (
-                <li key={todo}>{todo}</li>
+                <li key={todo.id}>{todo.name}</li>
             ))}
         </ul>
     </React.Fragment>
